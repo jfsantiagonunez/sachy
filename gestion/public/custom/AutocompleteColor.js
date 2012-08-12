aae3af0c8f0288061137ec0b985bf7412d09f14d
jfsantiagonunez <jfsantiagonunez@googlemail.com>
2012-08-12 17:05:57 -0400

    Adaptando a life

--- gestion/public/custom/AutocompleteColor.js
+++
@@ -1,9 +0,0 @@
-dojo.provide("custom.AutocompleteColor");
-dojo.require("dojox.data.QueryReadStore");
-dojo.declare("custom.AutocompleteColor", dojox.data.QueryReadStore, {
-    fetch:function (request) {
-		var calidad = dojo.byId('calidad').value;
-        request.serverQuery = { qcolor:request.query.color,qcalidad:calidad };
-        return this.inherited("fetch", arguments);
-    }
-});
